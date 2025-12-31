fdde5ab4b8611b8fdae1615090db7d195c6d5573
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-12-31 12:46:32 -0600

    more disclaimers

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -2,14 +2,19 @@
     
     
     <div class="bg-slate-700 w-240 mt-52 text-slate-200 rounded-lg p-3">
-        <div class="text-white relative -top-10 font-bold text-5xl">bubble network</div>
+        <div class="text-white relative -top-10 font-bold text-5xl">bubble network - info</div>
         
         <div>
             <h1>Info</h1>
-            <span>What is bubble network? It is a website that allows you to visualize how connected you are in the slack. By scanning all the public channels you are in, messages with interactions can be checked and visualized to see who you talk to.</span>
+            <span>What is bubble network? It is a website that lets you see how connected you are in the slack. By only looking at publicly accessible channels you are part of, patterns of interactions appear and can be visualized based on shared conversations.</span>
 
             <h1>Privacy Policy</h1>
-            <span>Only whats needed is stored. Things like your slack id, slack display name (fallbacks to real name if needed), and profile picture are stored. Your slack token and slack id are stored locally and are sent to api endpoints to be securely processed and are never stored on any servers/logs.</span>
+            <span>Only whats needed is stored. Things like your slack id, slack display name (fallbacks to real name if needed), and profile picture are stored. Your slack token and slack id are stored locally are never stored on any servers/logs.</span>
+            
+            <br>
+            <br>
+            
+            <span><b>tldr;</b> the information we access and need can be found without a oauth key. (anyone can request it as its public information)</span>
             
             <h1>Source Code</h1>
             <span>Made with svelte, tailwind, and fontawesome. Vercel and supabase are also used.</span>
@@ -23,6 +28,6 @@
     @reference "tailwindcss";
     
     h1 {
-        @apply text-2xl font-semibold pt-5 pb-2;
+        @apply text-2xl font-bold pt-5 pb-2;
     }
 </style>
