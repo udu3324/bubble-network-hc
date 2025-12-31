f3224ef1a3282670bc6ec5210e2872e8d14b4642
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-12-31 01:05:55 -0600

    visual stuff

--- src/routes/bar.svelte
+++ src/routes/bar.svelte
@@ -1,11 +1,13 @@
-<div class="bg-slate-900 mt-3 h-10 rounded-lg">
-    <button class="bg-green-800">save</button>
+<div class="bg-slate-900 mt-3 h-10 rounded-lg flex space-x-2">
+    <button class="bg-green-600"><i class="fa-solid fa-floppy-disk"></i></button>
+    <button class="bg-blue-600"><i class="fa-solid fa-arrows-rotate"></i></button>
 </div>
 
 <style lang="postcss">
     @reference "tailwindcss";
     
     button {
-        @apply text-white rounded-lg h-full px-3;
+        @apply text-white text-2xl rounded-lg h-full px-3 cursor-pointer;
+        @apply w-10 h-10 place-content-center text-center grid;
     }
 </style>
--- src/routes/viewer.svelte
+++ src/routes/viewer.svelte
@@ -1,3 +1,8 @@
-<div class="bg-slate-800 grow rounded-lg">
-    network visualizer here
+<div class="bg-slate-800 grow rounded-lg grid place-content-center">
+    <span class="text-slate-600 font-bold text-5xl text-center">network visualizer here</span>
 </div>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
+</style>
