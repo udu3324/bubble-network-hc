9c9f102e4821c02c1f925626e48a811a188151f9
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-01-07 21:44:26 -0600

    formatting

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -11,7 +11,8 @@ export async function foobar1() {
     const { data, error } = await supabase
         .from('network')
         .select()
-    return data;
+
+    return data
 }
 
 export async function foobar3() {
