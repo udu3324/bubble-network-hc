659e2da117b29da86f13743567ca0d90f7e18da9
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-01-17 21:33:56 -0600

    please work in production

--- src/routes/api/render/+server.js
+++ src/routes/api/render/+server.js
@@ -4,18 +4,14 @@ import { foobar1, foobar3 } from '$lib/supabaseClient'
 import { createCanvas, loadImage, registerFont } from 'canvas'
 
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 registerFont(
-    path.join(__dirname, "NebulaSans-Book.ttf"),
+    path.join(process.cwd(), "fonts/NebulaSans-Book.ttf"),
     { family: "Nebula Sans", weight: "normal" }
 )
 
 registerFont(
-    path.join(__dirname, "NebulaSans-Bold.ttf"),
+    path.join(process.cwd(), "fonts/NebulaSans-Bold.ttf"),
     { family: "Nebula Sans", weight: "bold" }
 )
 
