3df17346f4a1b11656e738f39f916ddf069c379c
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-01-25 19:20:39 -0600

    fix possible xss through avatar proxy endpoint

--- src/routes/api/slack/avatar/+server.js
+++ src/routes/api/slack/avatar/+server.js
@@ -2,12 +2,27 @@ export async function GET({ url }) {
     const thing = url.searchParams.get("url")
 
     if (!thing) {
+      return new Response(JSON.stringify({
+          error: "no url provided"
+      }), { status: 400 })
+    }
+
+    let parsed
+    try {
+      parsed = new URL(thing)
+    } catch {
+      return new Response(JSON.stringify({
+          error: "invalid url"
+      }), { status: 400 })
+    }
+
+    if (!(/^https:\/\/avatars\.slack-edge\.com\//.test(parsed.href) || /^https:\/\/secure\.gravatar\.com\//.test(parsed.href))) {
         return new Response(JSON.stringify({
-            error: "no id provided"
+            error: "unauthorized url"
         }), { status: 400 })
     }
 
-    const res = await fetch(thing)
+    const res = await fetch(parsed.href)
 
     if (!res.ok) {
       return new Response('Failed to fetch image', { status: 500 })
