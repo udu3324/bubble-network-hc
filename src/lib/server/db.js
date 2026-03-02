d721bbd3644d400d1d108c89ca8b4ba8f18884c7
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-03-01 20:05:35 -0600

    fix db.js

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,8 +1,14 @@
 import { DATABASE_URL } from '$env/static/private'
 import postgres from 'postgres'
 
-const sql = postgres(DATABASE_URL, {
-    ssl: process.env.NODE_ENV === 'production'
-})
+const globalForSQL = globalThis
+
+const sql =
+    globalForSQL.sql ||
+    postgres(DATABASE_URL, {
+        ssl: process.env.NODE_ENV === 'production',
+        max: 5,
+        idle_timeout: 20
+    })
 
 export default sql
